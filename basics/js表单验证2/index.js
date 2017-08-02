73a72a72094b524df008edee1399b3b1a6b73e1f
alisa <alisa@dxt.cn>
2017-08-02 16:53:45 +0800

    更改验证

--- basics/js表单验证2/index.js
+++ basics/js表单验证2/index.js
@@ -2,7 +2,8 @@
 /*提示信息*/
 var judgeArr = [{
         error1: "名称不能为空",
-        error2: "必填长度为4~16个字符",
+        error2: "名称格式错误",
+        error3: "必填长度为4~16个字符",
         success: "名称可用",
         judge: function(dom, domp) {
             if (GetLength2(dom.value) == 0) {
@@ -21,7 +22,8 @@ var judgeArr = [{
         }
     }, {
         error1: "密码不能为空",
-        error2: "必填密码最小长度为6个数组与字母组合",
+        error2: "密码格式不正确",
+        error3: "必填密码最小长度为6-20个数组与字母组合",
         success: "密码正确",
         judge: function(dom, domp) {
           /*密码正则*/
@@ -42,7 +44,8 @@ var judgeArr = [{
         }
     }, {
         error1: "密码确认不能为空",
-        error2: "必填再次输入相同的密码",
+        error2: "再次输入的密码与第一次输入密码不同",
+        error3: "必填再次输入相同的密码",
         success: "密码输入一致",
         judge: function(dom, domp) {
             if (!dom.value) {
@@ -62,10 +65,11 @@ var judgeArr = [{
     }, {
         error1: "邮箱不能为空",
         error2: "邮箱格式不正确",
+        error3: "邮箱格式,例如:12546@qq.com",
         success: "格式正确",
         judge: function(dom, domp) {
           /*密码正则*/
-          var pwdRex=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+          var pwdRex=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
             if (!dom.value) {
                 domp.className = "error";
                 domp.innerHTML = this.error1;
@@ -83,6 +87,7 @@ var judgeArr = [{
     }, {
         error1: "手机号不能为空",
         error2: "手机号格式不正确",
+        error3: "必填手机号由11个数字组成",
         success: "手机号正确",
         judge: function(dom, domp) {
           /*密码正则*/
@@ -112,7 +117,7 @@ for (var i = 0; i < inpts.length; i++) {
         var domP = this.nextElementSibling || this.nextSibling;
         domP.className = "";
         domP.style.display = "block";
-          domp.innerHTML=judgeArr[this.index].error2;
+        domP.innerHTML=judgeArr[this.index].error3;
     }
     inpts[i].onblur = function() {
         var domP = this.nextElementSibling || this.nextSibling;
